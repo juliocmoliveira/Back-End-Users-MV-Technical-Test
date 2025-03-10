2e6dd494bdb6e72dd2243f08a4e981d14c8c0dc8
juliocmoliveira <juliocesarele@gmail.com>
2025-03-10 02:35:05 -0300

    feat: create APIs of user

--- src/main/java/com/example/mv/infrastructure/config/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/mv/infrastructure/config/exceptions/UserNotFoundException.java
@@ -1,8 +1,6 @@
 package com.example.mv.infrastructure.config.exceptions;
 
-import jakarta.persistence.EntityNotFoundException;
-
-public class UserNotFoundException extends EntityNotFoundException {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
         super("User not found, verify id passed for requisition.");
     }
